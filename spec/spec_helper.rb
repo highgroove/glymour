a1cc1b6cab556dc88f02e93a8043f0cb51bc9166
Brian Stanwyck <brian.stanwyck@ncf.edu>
2011-09-06 16:14:12 -0400

    [Finishes #17931175] Incorporates code review suggestions in spec folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,50 @@ end
 def prob(p)
   rand < p
 end
+
+def alarm_init
+  @alarm_data = []
+  100000.times do
+    earthquake = prob(0.01)
+    burglary = prob(0.007)
+  
+    if burglary
+      alarm = earthquake ? prob(0.95) : prob(0.94)
+    else
+      alarm = earthquake ? prob(0.29) : prob(0.001)
+    end
+  
+    john_calls = alarm ? prob(0.90) : prob(0.05)
+    mary_calls = alarm ? prob(0.70) : prob(0.01)
+  
+    @alarm_data << { :e => earthquake, :b => burglary, :a => alarm, :j => john_calls, :m => mary_calls }
+  end
+  @e = Glymour::Statistics::Variable.new(@alarm_data, "Earthquake") { |r| r[:e] }
+  @b = Glymour::Statistics::Variable.new(@alarm_data, "Burglary") { |r| r[:b] }
+  @a = Glymour::Statistics::Variable.new(@alarm_data, "Alarm") { |r| r[:a] }
+  @j = Glymour::Statistics::Variable.new(@alarm_data, "John Calls") { |r| r[:j] }
+  @m = Glymour::Statistics::Variable.new(@alarm_data, "Mary Calls") { |r| r[:m] }
+  alarm_vars = [@e, @b, @a, @j, @m]
+
+  alarm_container = Glymour::Statistics::VariableContainer.new(@alarm_data, [@e, @b, @a, @j, @m])
+  @alarm_net = Glymour::StructureLearning::LearningNet.new(alarm_container)
+end
+
+def coin_init
+  # Highly simplified test net
+  # Only edges should be @h pointing to @red and @blue
+  @coin_data = []
+  10000.times do
+    h = prob(0.5)
+    red = h ? prob(0.2) : prob(0.7)
+    blue = h ? prob(0.4) : prob(0.9)
+    @coin_data << { :h => h, :red => red, :blue => blue }
+  end
+  
+  @h = Glymour::Statistics::Variable.new(@coin_data, "Heads") { |r| r[:h] }
+  @red = Glymour::Statistics::Variable.new(@coin_data, "Red") { |r| r[:red] }
+  @blue = Glymour::Statistics::Variable.new(@coin_data, "Blue") { |r| r[:blue] }
+  
+  coin_container = Glymour::Statistics::VariableContainer.new(@coin_data, [@h, @red, @blue])
+  @coin_net = Glymour::StructureLearning::LearningNet.new(coin_container)
+end
--- spec/statistics_spec.rb
+++ spec/statistics_spec.rb
@@ -8,38 +8,8 @@ describe Glymour::Statistics do
     R.echo(true)
     Stats = StatsDummy.new
     
-    @alarm_data = []
-    100000.times do
-      e = prob(0.002)
-      b = prob(0.001)
-      a = b ? (e ? prob(0.95) : prob(0.94)) : (e ? prob(0.29) : prob(0.001))
-      j = a ? prob(0.90) : prob(0.05)
-      m = a ? prob(0.70) : prob(0.01)
-      @alarm_data << { :e => e, :b => b, :a => a, :j => j, :m => m}
-    end
-    @e = Glymour::Statistics::Variable.new(@alarm_data) { |r| r[:e] }
-    @b = Glymour::Statistics::Variable.new(@alarm_data) { |r| r[:b] }
-    @a = Glymour::Statistics::Variable.new(@alarm_data) { |r| r[:a] }
-    @j = Glymour::Statistics::Variable.new(@alarm_data) { |r| r[:j] }
-    @m = Glymour::Statistics::Variable.new(@alarm_data) { |r| r[:m] }
-    
-    alarm_container = Glymour::Statistics::VariableContainer.new(@alarm_data, [@e, @b, @a, @j, @m])
-    @alarm_net = Glymour::StructureLearning::LearningNet.new(alarm_container)
-    
-    @coin_data = []
-    10000.times do
-      h = prob(0.5)
-      red = h ? prob(0.2) : prob(0.7)
-      blue = h ? prob(0.5) : prob(0.9)
-      @coin_data << { :h => h, :red => red, :blue => blue }
-    end
-    
-    @h = Glymour::Statistics::Variable.new(@coin_data) { |r| r[:h] }
-    @red = Glymour::Statistics::Variable.new(@coin_data) { |r| r[:red] }
-    @blue = Glymour::Statistics::Variable.new(@coin_data) { |r| r[:blue] }
-    
-    coin_container = Glymour::Statistics::VariableContainer.new(@coin_data, [@h, @red, @blue])
-    @coin_net = Glymour::StructureLearning::LearningNet.new(coin_container)
+    alarm_init
+    coin_init
   end
   
   it 'should give chi square independence data for two variables' do
